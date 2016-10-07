e011a6ab736886321a5d0adf5f365d43745d45a2
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2016-10-08 00:47:02 +0200

    Implement Reinforcement Learning

--- src/reactive/DataStructures.java
+++ src/reactive/DataStructures.java
@@ -3,7 +3,9 @@ package reactive;
 import logist.task.TaskDistribution;
 import logist.topology.Topology.City;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 enum ActionType {
     MOVE,
@@ -20,6 +22,58 @@ class State {
         this.neighbours = n;
     }
 
+    static Set<State> generateAllStates(List<City> cities) {
+        Set<State> S = new HashSet<>();
+
+        for (City current : cities) {
+            // add also null destinations
+            S.add(new State(current, null, current.neighbors()));
+            for (City destination : cities) {
+                if (current != destination) {
+                    S.add(new State(current, destination, current.neighbors()));
+                }
+            }
+        }
+        return S;
+    }
+
+    static Set<State> generateAllNextStates(State current, ReactiveAction action, List<City> cities) {
+        Set<State> S = new HashSet<>();
+        City destination = action.getDestination();
+
+        // state where there is no task present
+        S.add(new State(destination, null, destination.neighbors()));
+
+        // add state where there is a task for all possible cities
+        for (City taskDestination : cities) {
+            if (taskDestination != destination) {
+                S.add(new State(destination, taskDestination, destination.neighbors()));
+            }
+        }
+        return S;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        State state = (State) o;
+
+        if (!currentCity.equals(state.currentCity)) return false;
+        if (taskDestination != null ? !taskDestination.equals(state.taskDestination) : state.taskDestination != null)
+            return false;
+        return neighbours.equals(state.neighbours);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = currentCity.hashCode();
+        result = 31 * result + (taskDestination != null ? taskDestination.hashCode() : 0);
+        result = 31 * result + neighbours.hashCode();
+        return result;
+    }
+
     City getCurrentCity() {
         return currentCity;
     }
@@ -33,13 +87,87 @@ class State {
     }
 }
 
+class ActionValue {
+    private ReactiveAction action;
+    private Double value;
+
+    ActionValue(ReactiveAction action, Double value) {
+        this.action = action;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ActionValue that = (ActionValue) o;
+
+        if (!action.equals(that.action)) return false;
+        return value.equals(that.value);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = action.hashCode();
+        result = 31 * result + value.hashCode();
+        return result;
+    }
+
+    ReactiveAction getAction() {
+        return action;
+    }
+
+    Double getValue() {
+        return value;
+    }
+}
+
 class ReactiveAction {
     private ActionType type;
     private City destination;
 
-    public ReactiveAction(ActionType type, City destination) {
+    ReactiveAction(ActionType type, City destination) {
         this.type = type;
         this.destination = destination;
+    }
+
+    static Set<ReactiveAction> generateActionsForState(List<City> cities, State s) {
+        Set<ReactiveAction> A = new HashSet<>();
+        for (ActionType type : ActionType.values()) {
+            if (type == ActionType.MOVE) {
+                // only generate Moves to neighbours of the current city
+                for (City destination : s.getCurrentCity().neighbors()) {
+                    A.add(new ReactiveAction(type, destination));
+                }
+            } else if (type == ActionType.PICKUP) {
+                // only generate pickup action if there is a task avalaible
+                if (s.getTaskDestination() != null) {
+                    A.add(new ReactiveAction(type, s.getTaskDestination()));
+                }
+            }
+        }
+        return A;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ReactiveAction that = (ReactiveAction) o;
+
+        if (type != that.type) return false;
+        return destination.equals(that.destination);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type.hashCode();
+        result = 31 * result + destination.hashCode();
+        return result;
     }
 
     City getDestination() {
@@ -54,11 +182,11 @@ class ReactiveAction {
 class RewardTable {
     private TaskDistribution td;
 
-    public RewardTable(TaskDistribution td) {
+    RewardTable(TaskDistribution td) {
         this.td = td;
     }
 
-    public double getReward(State s, ReactiveAction a) {
+    double getReward(State s, ReactiveAction a) {
         City currentCity = s.getCurrentCity();
         City destination = a.getDestination();
 
@@ -80,12 +208,12 @@ class ProbabilityTransitionTable {
     private List<City> cities;
     private TaskDistribution td;
 
-    public ProbabilityTransitionTable(List<City> cities, TaskDistribution td) {
+    ProbabilityTransitionTable(List<City> cities, TaskDistribution td) {
         this.cities = cities;
         this.td = td;
     }
 
-    public double getProbability(State initialState, ReactiveAction a, State endState) {
+    double getProbability(State initialState, ReactiveAction a, State endState) {
         City destination = a.getDestination();
         City taskDestination = endState.getTaskDestination();
 
@@ -107,35 +235,3 @@ class ProbabilityTransitionTable {
         return 1 - acc;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/reactive/ReactiveAgent.java
+++ src/reactive/ReactiveAgent.java
@@ -9,16 +9,23 @@ import logist.simulation.Vehicle;
 import logist.task.Task;
 import logist.task.TaskDistribution;
 import logist.topology.Topology;
-import logist.topology.Topology.City;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 public class ReactiveAgent implements ReactiveBehavior {
 
 	private Random random;
-	private double pPickup;
-	private int numActions;
-	private Agent myAgent;
+    private double discount;
+    private int numActions;
+    private Agent myAgent;
+    private Topology topology;
+    private TaskDistribution td;
+
+    // best action to take associated with each state
+    private Map<State, ActionValue> V;
 
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
@@ -29,22 +36,41 @@ public class ReactiveAgent implements ReactiveBehavior {
 				0.95);
 
 		this.random = new Random();
-		this.pPickup = discount;
-		this.numActions = 0;
-		this.myAgent = agent;
-	}
+        this.discount = discount;
+        this.numActions = 0;
+        this.myAgent = agent;
+        this.topology = topology;
+        this.V = new HashMap<>();
+        this.td = td;
+
+        reinforcementLearning();
+    }
 
 	@Override
 	public Action act(Vehicle vehicle, Task availableTask) {
 		Action action;
 
-		if (availableTask == null || random.nextDouble() > pPickup) {
-			City currentCity = vehicle.getCurrentCity();
-			action = new Move(currentCity.randomNeighbor(random));
-		} else {
-			action = new Pickup(availableTask);
-		}
-		
+        State currentState = new State(vehicle.getCurrentCity()
+                , availableTask == null ? null : availableTask.deliveryCity
+                , vehicle.getCurrentCity().neighbors());
+
+        ReactiveAction bestAction = V.get(currentState).getAction();
+
+        switch (bestAction.getType()) {
+            case MOVE:
+                action = new Move(bestAction.getDestination());
+                break;
+            case PICKUP:
+                if (availableTask != null) {
+                    action = new Pickup(availableTask);
+                } else {
+                    action = new Move(bestAction.getDestination());
+                }
+                break;
+            default:
+                action = null;
+        }
+
 		if (numActions >= 1) {
 			System.out.println("The total profit after "+numActions+" actions is "+myAgent.getTotalProfit()+" (average profit: "+(myAgent.getTotalProfit() / (double)numActions)+")");
 		}
@@ -52,4 +78,62 @@ public class ReactiveAgent implements ReactiveBehavior {
 		
 		return action;
 	}
+
+    private void reinforcementLearning() {
+        boolean goodEnough = false;
+
+        // get all possible states
+        Set<State> states = State.generateAllStates(topology.cities());
+
+        // initialize V
+        for (State s : states) {
+            V.put(s, new ActionValue(null, 0.0));
+        }
+
+        RewardTable rewardTable = new RewardTable(td);
+        ProbabilityTransitionTable p = new ProbabilityTransitionTable(topology.cities(), td);
+
+        // create Q table and initialize it
+        Map<State, Map<ReactiveAction, Double>> Q = new HashMap<>();
+        for (State s : states) {
+            // initialize inner hashmap
+            Q.put(s, new HashMap<>());
+            for (ReactiveAction a : ReactiveAction.generateActionsForState(topology.cities(), s)) {
+                Q.get(s).put(a, 0.0);
+            }
+        }
+
+        while (!goodEnough) {
+            Double maxValueDifference = 0.0;
+            for (State s : Q.keySet()) {
+                for (ReactiveAction a : Q.get(s).keySet()) {
+                    double qValue = rewardTable.getReward(s, a);
+
+                    // go through all possible state transitions
+                    for (State nextState : State.generateAllNextStates(s, a, topology.cities())) {
+                        qValue += discount * p.getProbability(s, a, nextState);
+                    }
+
+                    Q.get(s).put(a, qValue);
+                }
+                double bestValue = -Double.MAX_VALUE;
+                ReactiveAction bestAction = null;
+                for (ReactiveAction a : Q.get(s).keySet()) {
+                    double currentValue = Q.get(s).get(a);
+                    if (currentValue >= bestValue) {
+                        bestValue = currentValue;
+                        bestAction = a;
+                    }
+                }
+                Double valueDifference = Math.abs(V.get(s).getValue() - bestValue);
+                maxValueDifference = Math.max(valueDifference, maxValueDifference);
+
+                V.put(s, new ActionValue(bestAction, bestValue));
+            }
+            // stop when difference between successive iterations is less than a given value
+            if (maxValueDifference < 0.1) {
+                goodEnough = true;
+            }
+        }
+    }
 }
